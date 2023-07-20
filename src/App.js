b737e6d487f79d6c5ba0fcff1b0554917ed9bfb6
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2023-07-20 15:09:44 +0530

    login css fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {Login} from './components/Login';
+import { Login } from './components/Login/Login';
+
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Login/>
     </div>
   );
 }
