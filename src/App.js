1919ba8a55ed508b666063135d3db21a0c8e4a55
simmithapad <simmithapad23@gmail.com>
2023-07-20 18:12:32 +0530

    webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Timer from './components/solo/Timer';
 // import { Login } from './components/Login/Login';
-import Dropdown from './components/solo/Dropdown';
+// import Dropdown from './components/solo/Dropdown';
 
 function App() {
   return (
     <div className="App">
-      <Dropdown/>
+      <Timer/>
     </div>
   );
 }
