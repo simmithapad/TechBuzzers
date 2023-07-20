90c721af9f3b16df4ebac3b7c99cac701595285f
Aastha Singhal <aastha.tanya21@gmail.com>
2023-07-20 18:32:07 +0530

    mode page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { Login } from './components/Login/Login';
+//import { Login } from './components/Login/Login';
+import PlayeMode from './components/PlayerMode/PlayeMode';
+
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <PlayeMode/>
     </div>
   );
 }
---
+++ src/components/PlayerMode/PlayeMode.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./PlayerMode.css";
+export default function PlayeMode() {
+  return (
+    <div className="modepage">
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <h1>Play Solo</h1>
+    </a>
+    <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <h1>Play With Friends</h1>
+    </a>
+    </div>
+  )
+}
