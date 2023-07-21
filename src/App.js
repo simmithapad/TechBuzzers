c44144c95f496572089f2971f99aad6d3f646f10
Aastha Singhal <aastha.tanya21@gmail.com>
2023-07-21 09:44:44 +0530

    Lobby Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Practice from './components/practice/Practice';
 import PlayeMode from './components/PlayerMode/PlayeMode';
+import FlipCard from './components/multi/FlipCard';
 import {
   BrowserRouter as Router,
   Route,
@@ -19,6 +20,7 @@ function App() {
           <Route path="/mode" element={<PlayeMode/>}/>
           <Route path="/" element={<Login/>}/>
           <Route path="/solo" element={<Practice/>}/>
+          <Route path="/Lobby" element={<FlipCard/>}/>
         </Routes>
         </Router>
     </div>
